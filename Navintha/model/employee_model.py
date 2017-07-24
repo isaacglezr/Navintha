effe974a78fd0e872eeec5407cdb015fe77e4cf1
isaacglezr <igr@soluciones4g.com>
2017-07-24 18:41:45 -0500

    [REF]Navinta Employee: Adding new fields

---
+++ Navintha/model/employee_model.py
@@ -0,0 +1,13 @@
+from openerp import fields, models, api
+
+class EmployeeN(models.Model):
+
+    _name = 'Employee.Navintha'
+
+    x_numempleado = fields.Char(string="Numero de Empleado")
+    x_contratacion = fields.Date(string="Fecha de Contratacion")
+    x_antiguedad = fields.Integer(string="Antiguedad", readonly=True)
+    x_perfilacademico = fields.Many2one('hr.escolaridad', string="Perfil Academico")
+
+
+
