45d18dd95a548ef5fe22e0ccb7debd0f1e509dc6
isaacglezr <igr@soluciones4g.com>
2017-07-24 19:40:43 -0500

    [IMP]Nuevo

--- Employee_Navintha/model/employee_model.py
+++ Employee_Navintha/model/employee_model.py
@@ -2,7 +2,7 @@ from openerp import fields, models, api
 
 class EmployeeN(models.Model):
 
-    _name = 'Employee.Navintha'
+    _name = 'employee.navintha'
 
     x_numempleado = fields.Char(string="Numero de Empleado")
     x_contratacion = fields.Date(string="Fecha de Contratacion")
